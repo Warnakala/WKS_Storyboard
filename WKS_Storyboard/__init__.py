d98d86243ce40f03be4048387657027274008573
Adhi Hargo <cadmus.sw@gmail.com>
2021-09-10 11:35:46 +0700

    Basic initialization script

--- WKS_Storyboard/__init__.py
+++ WKS_Storyboard/__init__.py
@@ -17,3 +17,58 @@
 # ##### END GPL LICENSE BLOCK #####
 
 # Initialization script for WKS Storyboard template
+
+import bpy
+from bpy.app.handlers import persistent
+
+
+@persistent
+def load_handler(dummy):
+    import bpy
+
+    # 2D Animation
+    screen = bpy.data.screens['2D Animation']
+    if screen:
+        for area in screen.areas:
+            # Set Tool settings as default in properties panel.
+            if area.type == 'PROPERTIES':
+                for space in area.spaces:
+                    if space.type != 'PROPERTIES':
+                        continue
+                    space.context = 'TOOL'
+
+            # Open sidebar in Dopesheet.
+            elif area.type == 'DOPESHEET_EDITOR':
+                for space in area.spaces:
+                    if space.type != 'DOPESHEET_EDITOR':
+                        continue
+                    space.show_region_ui = True
+
+    # 2D Full Canvas
+    screen = bpy.data.screens['2D Full Canvas']
+    if screen:
+        for area in screen.areas:
+            if area.type == 'VIEW_3D':
+                for space in area.spaces:
+                    if space.type != 'VIEW_3D':
+                        continue
+                    space.shading.type = 'MATERIAL'
+                    space.shading.use_scene_world = True
+
+    # Grease pencil object
+    scene = bpy.data.scenes[0]
+    if scene:
+        scene.tool_settings.use_keyframe_insert_auto = True
+        for ob in scene.objects:
+            if ob.type == 'GPENCIL':
+                gpd = ob.data
+                gpd.onion_keyframe_type = 'ALL'
+
+def register():
+    logger.debug("Registering module")
+    bpy.app.handlers.load_factory_startup_post.append(load_handler)
+
+
+def unregister():
+    logger.debug("Unregistering module")
+    bpy.app.handlers.load_factory_startup_post.remove(load_handler)
